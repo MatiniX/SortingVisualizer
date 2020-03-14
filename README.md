e4308180e155566b356b0f9cf290c7b1dfa7f872
MatiniX <matinix@users.noreply.github.com>
2020-03-14 11:36:37 +0100

    Updates

--- README.md
+++
@@ -1,8 +0,0 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-## O projekte
-
-React.js SPA aplikácia vytvorená Martinom Michálikom do súťaže JuniorInternet. Toto repo je verejné. Kód nie je komentovaný na 100%. Celú aplikáciu je možno 
-naklonovať a použiť kód na vlastné účely. V takom prípade ale nevýdavajte aplikáciu za vlastnú a označte autora (najlepšie linkom na toto repo).
-
-
---
+++ precache-manifest.ce966cfa14b194a2bf32bf2cc146bcc9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f717ee6973d866f6e39bbad83ce96780",
+    "url": "/SortingVisualizer/index.html"
+  },
+  {
+    "revision": "2dd087d83aa060bd08cf",
+    "url": "/SortingVisualizer/static/css/main.d22d3b7e.chunk.css"
+  },
+  {
+    "revision": "c36598bd30ce78fce17a",
+    "url": "/SortingVisualizer/static/js/2.548744c9.chunk.js"
+  },
+  {
+    "revision": "192201c36557064462a543b746126438",
+    "url": "/SortingVisualizer/static/js/2.548744c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dd087d83aa060bd08cf",
+    "url": "/SortingVisualizer/static/js/main.9d1ebc6c.chunk.js"
+  },
+  {
+    "revision": "f979f2e5d7f9f67788a8",
+    "url": "/SortingVisualizer/static/js/runtime-main.2fb23952.js"
+  },
+  {
+    "revision": "d4c88b8cc620af6af67c33910899fcf7",
+    "url": "/SortingVisualizer/static/media/Bubble-sort-example-300px.d4c88b8c.gif"
+  },
+  {
+    "revision": "6e67d1c722106442b422ee53e98575b3",
+    "url": "/SortingVisualizer/static/media/Insertion-sort-example-300px.6e67d1c7.gif"
+  },
+  {
+    "revision": "a29c0dd0186d1f8cef3c5ebdedf3e5a3",
+    "url": "/SortingVisualizer/static/media/Merge-sort-example-300px.a29c0dd0.gif"
+  },
+  {
+    "revision": "f20b8898585b3ca03843d93ce2c35a68",
+    "url": "/SortingVisualizer/static/media/Selection-Sort-Animation.f20b8898.gif"
+  },
+  {
+    "revision": "d4e5d0a778dba725091d8317e6bac939",
+    "url": "/SortingVisualizer/static/media/Sorting_quicksort_anim.d4e5d0a7.gif"
+  }
+]);
